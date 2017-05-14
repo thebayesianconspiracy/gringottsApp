2488a03da2b061c81d17dc410dcf82c318235338
Aditya Agarwal <aditya.kgp25@gmail.com>
2017-05-14 11:49:38 +0530

    added sample card view with lists

--- app/src/main/java/com/willblaschko/android/alexavoicelibrary/MainActivity.java
+++ app/src/main/java/com/willblaschko/android/alexavoicelibrary/MainActivity.java
@@ -5,12 +5,19 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.willblaschko.android.alexavoicelibrary.actions.ActionsFragment;
 import com.willblaschko.android.alexavoicelibrary.actions.BaseListenerFragment;
 import com.willblaschko.android.alexavoicelibrary.actions.SendAudioActionFragment;
+
+import org.w3c.dom.Text;
 
 import static com.willblaschko.android.alexavoicelibrary.R.id.frame;
 
@@ -24,6 +31,8 @@ public class MainActivity extends BaseActivity implements ActionsFragment.Action
     private View statusBar;
     private TextView status;
     private View loading;
+
+    private RecyclerView mRecyclerView;
 
 
     @Override
@@ -44,6 +53,24 @@ public class MainActivity extends BaseActivity implements ActionsFragment.Action
         //ActionsFragment fragment = new ActionsFragment();
         loadFragment(new SendAudioActionFragment(), false);
         //loadFragment(fragment, false);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        String[] myDataset = new String[2];
+        myDataset[0] = "String 0 ";
+        myDataset[1] = "String 1";
+        MyAdapter mAdapter = new MyAdapter(myDataset);
+
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
@@ -133,4 +160,55 @@ public class MainActivity extends BaseActivity implements ActionsFragment.Action
         getSupportFragmentManager().popBackStack();
         return true;
     }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private String[] mDataset;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public LinearLayout mTextView;
+            public ViewHolder(LinearLayout v) {
+                super(v);
+                mTextView = v;
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyAdapter(String[] myDataset) {
+            mDataset = myDataset;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+            // create a new view
+            LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.cardview, parent, false);
+
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            TextView textView = (TextView) holder.mTextView.findViewById(R.id.info_text);
+            textView.setText(mDataset[position]);
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
+    }
+
+
 }
